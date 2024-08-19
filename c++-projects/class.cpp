b94c73536352b9f8112a325536c9386fbb4d6996
noumecha spaker <noumechaivan@gmail.com>
2024-08-19 13:04:14 +0100

    new stuff for learners

--- c++-projects/class.cpp
+++ c++-projects/class.cpp
@@ -10,10 +10,41 @@ class Voiture {
             cout << "Constructor called" << endl;
         }
 };
+class Rectangle {
+    private:
+        double lon;
+        double lar;
+    public:
+        Rectangle() {
+            cout << "Constructor called" << endl;
+        };
+        double perim() {
+            return (lon+lar)*2;
+        };
+        double surf() {
+            return lon * lar;
+        };
+        void setLon(double x) {
+            lon = x;
+        };
+        void setLar(double y) {
+            lar = y;
+        };
+        double getLon() {
+            return lon;
+        };
+        double getLar() {
+            return lar;
+        };
+};
 int main() {
     Voiture v;
     v.name = "Ferrari";
     cout << "Voiture 1 name : " << v.name << endl;
-
+    Rectangle r;
+    r.setLon(4);
+    r.setLar(5);
+    cout << "Perimètre du rectangle : " << r.perim() << " cm" << endl;
+    cout << "Aire du rectangle : " << r.surf() << " cm2" << endl;
     return 0;
 }
